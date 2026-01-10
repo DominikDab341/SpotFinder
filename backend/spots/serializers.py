19e506e9102d18c6be1d5236eb32cfbaee8389b2
unknown <281053@student.pwr.edu.pl>
2026-01-10 15:28:19 +0100

    refactor: apply DRY for Spot creation and reservation and add atomic transaction

--- backend/spots/serializers.py
+++ backend/spots/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Reservation, Spot, FavoriteSpot
+from django.db import transaction
 
 class SpotSearchSerializer(serializers.Serializer):
     address = serializers.CharField(required=True)
@@ -13,14 +14,29 @@ class SpotDisplayMetaSerializer(serializers.ModelSerializer):
             'google_place_id', 'name', 'address', 'spot_type',
         ]
 
-class ReservationSerializer(serializers.ModelSerializer):
-    #input
+class SpotGetOrCreate(serializers.Serializer):
     google_place_id = serializers.CharField(write_only=True)
     name = serializers.CharField(write_only=True)
     address = serializers.CharField(write_only=True)
     spot_type = serializers.CharField(write_only=True, required=False, allow_blank=True)
 
-    #output
+    def get_or_create_spot(self,validated_data):
+        google_id = validated_data.pop('google_place_id')
+        name = validated_data.pop('name')
+        address = validated_data.pop('address')
+        spot_type = validated_data.pop('spot_type', 'unknown')
+
+        spot, created = Spot.objects.get_or_create(
+            google_place_id=google_id,
+            defaults={
+                'name': name,
+                'address': address,
+                'spot_type': spot_type
+            }
+        )
+        return spot
+
+class ReservationSerializer(SpotGetOrCreate,serializers.ModelSerializer):
     spot_details = SpotDisplayMetaSerializer(source='spot', read_only=True)
 
     class Meta:
@@ -29,62 +45,36 @@ class ReservationSerializer(serializers.ModelSerializer):
             'google_place_id', 'name', 'address', 'spot_type', 'spot_details'
         ]
         read_only_fields = ['id', 'status', 'user']
+    def create(self,validated_data):
+        with transaction.atomic():
+            spot = self.get_or_create_spot(validated_data)
 
-    def create(self, validated_data):
-        google_id = validated_data.pop('google_place_id')
-        name = validated_data.pop('name')
-        address = validated_data.pop('address')
-        spot_type = validated_data.pop('spot_type', 'unknown')
-
-        spot, created = Spot.objects.get_or_create(
-            google_place_id=google_id,
-            defaults={
-                'name': name,
-                'address': address,
-                'spot_type': spot_type
-            }
-        )
-        reservation = Reservation.objects.create(
-            spot=spot,
-        )
-        
-        return reservation
+            reservation = Reservation.objects.create(
+                spot=spot,
+                **validated_data
+            )
+            
+            return reservation
     
 
-class FavoriteSpotSerializer(serializers.ModelSerializer):
-    #input
-    google_place_id = serializers.CharField(write_only=True)
-    name = serializers.CharField(write_only=True)
-    address = serializers.CharField(write_only=True)
-    spot_type = serializers.CharField(write_only=True, required=False, allow_blank=True)
-
-    #output
+class FavoriteSpotSerializer(SpotGetOrCreate, serializers.ModelSerializer):
     spot_details = SpotDisplayMetaSerializer(source='spot', read_only=True)
 
     class Meta:
         model = FavoriteSpot
         fields = [
-            'id', 'google_place_id',  'name', 'address', 'spot_type', 'spot_details'
+            'id', 'google_place_id', 'name', 'address', 'spot_type', 'spot_details'
         ]
 
-    def create(self, validated_data):   
-        user = validated_data.pop('user')
-        google_id = validated_data.pop('google_place_id')
-        name = validated_data.pop('name')
-        address = validated_data.pop('address')
-        spot_type = validated_data.pop('spot_type', 'unknown')
+    def create(self, validated_data):  
 
-        spot, created = Spot.objects.get_or_create(
-            google_place_id=google_id,
-            defaults={
-                'name': name,
-                'address': address,
-                'spot_type': spot_type
-            }
-        )
-        favorite_spot, created = FavoriteSpot.objects.get_or_create(
-            spot=spot,
-            user=user,
-        )
-        
-        return favorite_spot
+        with transaction.atomic():
+            spot = self.get_or_create_spot(validated_data)
+            user = validated_data.pop('user')
+
+            favorite_spot, created = FavoriteSpot.objects.get_or_create(
+                spot=spot,
+                user=user,
+            )
+            
+            return favorite_spot
