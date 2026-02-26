e01aa26e03f5b12602cd7a63f76176ebd1db709f
Dominik Dąbrowski <281053@student.pwr.edu.pl>
2026-02-26 20:52:48 +0100

    feat(frontend): add spot to favorites

--- frontend/src/components/SpotCard.tsx
+++ frontend/src/components/SpotCard.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import api from '../api/api';
 
 export interface DisplayName {
     text: string;
@@ -12,6 +14,7 @@ export interface Spot {
     rating?: number;
     userRatingCount?: number;
     priceLevel?: string;
+    spot_type?: string;
 }
 
 export interface SpotCardProps {
@@ -19,6 +22,35 @@ export interface SpotCardProps {
 }
 
 function SpotCard({spot}: SpotCardProps) {
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    //TODO: Fix removing from favorites
+    const handleRemoveFromFavorites = async () => {
+        if (!isFavorite) return;
+
+        try {
+            const response = await api.delete(`favorites/${spot.id}/`);
+            setIsFavorite(false);
+        } catch (error) {
+            console.error('Error removing from favorites:', error);
+        }
+    };
+    const handleAddToFavorites = async () => {
+        if (isFavorite) return;
+
+        try {
+            const response = await api.post('favorites/', {
+                google_place_id: spot.id,
+                name: spot.displayName.text,
+                address: spot.formattedAddress,
+                spot_type: spot.spot_type || "unknown"
+                //TODO: Consider whether this is a good approach
+            });
+            setIsFavorite(true);
+        } catch (error) {
+            console.error('Error adding to favorites:', error);
+        }
+    };
     return (
         <div>
             <h1>{spot.displayName.text}</h1>
@@ -26,6 +58,16 @@ function SpotCard({spot}: SpotCardProps) {
             <p>Rating: {spot.rating}</p>
             <p>User Rating Count: {spot.userRatingCount}</p>
             <p>Price Level: {spot.priceLevel ?? "No data available"}</p>
+            {isFavorite ? (
+                <button onClick={handleRemoveFromFavorites}>
+                    Remove from favorites 💔
+                </button>
+            ) : (
+                <button onClick={handleAddToFavorites}>
+                    Add to favorites ❤️
+                </button>
+            )}
+
         </div>
     );
 }
